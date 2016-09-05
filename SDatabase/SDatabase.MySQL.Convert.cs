7e04a32841bb0bd9277956eb0a1efbeb8efd9d95
Sty BL <stysdaman@gmail.com>
2016-09-05 20:32:32 +0300

    Implemented object deserializer.

---
+++ SDatabase/SDatabase.MySQL.Convert.cs
@@ -0,0 +1,43 @@
+﻿namespace SDatabase.MySQL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MySql.Data.MySqlClient;
+
+    public static class Convert
+    {
+        public static T DeserializeObject<T>(MySqlCommand cmd)
+        { 
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+
+                    var types = Enumerable.Range(0, reader.FieldCount).Select(reader.GetFieldType).ToArray();
+
+                    var constructor = typeof(T).GetConstructor(types);
+
+                    var passParams = new List<object>();
+
+                    foreach (var param in constructor.GetParameters())
+                    {
+                        foreach (var column in columns)
+                        {
+                            if (column == param.Name)
+                            {
+                                passParams.Add(reader[column]);
+                            }
+                        }
+                    }
+
+                    return (T)Activator.CreateInstance(typeof(T), passParams.ToArray());
+                }
+            }
+            return (T)Activator.CreateInstance(typeof(T));
+        }
+    }
+}
