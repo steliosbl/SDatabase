c30823ede754204c474641eed1a2e6bcd4c1381a
Sty BL <stysdaman@gmail.com>
2016-10-03 23:01:27 +0300

    Implemented proper list conversion for MySQL (untested).

--- SDatabase/SDatabase.MySQL.Convert.cs
+++ SDatabase/SDatabase.MySQL.Convert.cs
@@ -29,9 +29,11 @@ namespace SDatabase.MySQL
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.Text;
     using System.Threading.Tasks;
     using MySql.Data.MySqlClient;
+    using Newtonsoft.Json;
 
     /// <summary>
     /// Contains conversion methods to write and read C# objects from MySQL databases.
@@ -45,7 +47,7 @@ namespace SDatabase.MySQL
         /// <param name="cmd">The query used to acquire data.</param>
         /// <returns>An instance of the specified type with all the data acquired from the server.</returns>
         public static T DeserializeObject<T>(MySqlCommand cmd)
-        { 
+        {
             // User error checks:
             if (string.IsNullOrWhiteSpace(cmd.CommandText) || cmd.CommandText == string.Empty)
             {
@@ -64,11 +66,15 @@ namespace SDatabase.MySQL
             {
                 while (reader.Read())
                 {
-                    var columns = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
+                    var columnNames = Enumerable.Range(0, reader.FieldCount).Select(reader.GetName).ToList();
 
-                    var types = Enumerable.Range(0, reader.FieldCount).Select(reader.GetFieldType).ToArray();
+                    var columnTypes = Enumerable.Range(0, reader.FieldCount).Select(reader.GetFieldType).ToList();
 
-                    var constructor = typeof(T).GetConstructor(types);
+                    var columns = columnNames.Zip(columnTypes, (k, v) => new { Key = k, Value = v })
+                     .ToDictionary(x => x.Key, x => x.Value);
+
+                    var constructor = GetIdealConstructor<T>(columns);
+
                     if (constructor == null)
                     {
                         throw new ArgumentException("Type does not have a matching constructor!", "constructor");
@@ -78,11 +84,18 @@ namespace SDatabase.MySQL
 
                     foreach (var param in constructor.GetParameters())
                     {
-                        foreach (var column in columns)
+                        foreach (var column in columns.Keys)
                         {
                             if (column.Equals(param.Name, StringComparison.InvariantCultureIgnoreCase))
                             {
-                                passParams.Add(reader[column]);
+                                if (param.ParameterType.Name == "List`1")
+                                {
+                                    passParams.Add(JsonConvert.DeserializeObject(reader[column].ToString(), param.ParameterType));
+                                }
+                                else
+                                {
+                                    passParams.Add(reader[column]);
+                                }
                             }
                         }
                     }
@@ -134,7 +147,14 @@ namespace SDatabase.MySQL
                     cmd.Prepare();
                     foreach (var property in properties)
                     {
-                        cmd.Parameters.AddWithValue("@" + property.Name, property.GetValue(obj));
+                        var value = property.GetValue(obj);
+
+                        if (property.PropertyType.Name == "List`1")
+                        {
+                            value = JsonConvert.SerializeObject(value);
+                        }
+
+                        cmd.Parameters.AddWithValue("@" + property.Name, value);
                     }
 
                     cmd.ExecuteNonQuery();
@@ -168,6 +188,53 @@ namespace SDatabase.MySQL
             cmdstr = cmdstr.Remove(cmdstr.Length - 2);
             cmdstr += ");";
             return cmdstr;
+        }
+
+        /// <summary>
+        /// Finds the ideal constructor to use given a dictionary containing the names and types of a set of columns.
+        /// </summary>
+        /// <typeparam name="T">The type for which to find the constructor.</typeparam>
+        /// <param name="columns">Dictionary containing the names (key) and types (value) of a set of columns.</param>
+        /// <returns>The ideal constructor to use.</returns>
+        public static ConstructorInfo GetIdealConstructor<T>(Dictionary<string, Type> columns)
+        {
+            int maxCommon = 0;
+            var constructors = new List<ConstructorInfo>();
+            foreach (var constructor in typeof(T).GetConstructors())
+            {
+                var parameters = new Dictionary<string, Type>();
+                foreach (var parameter in constructor.GetParameters())
+                {
+                    if (parameter.ParameterType.Name == "List`1")
+                    {
+                        parameters.Add(parameter.Name, typeof(string));
+                    }
+                    else
+                    {
+                        parameters.Add(parameter.Name, parameter.ParameterType);
+                    }
+                }
+
+                int temp = columns.Keys.ToList().Intersect(parameters.Keys.ToList(), StringComparer.InvariantCultureIgnoreCase).Select(param => parameters[param] == columns[param]).Count();
+                if (temp > maxCommon)
+                {
+                    maxCommon = temp;
+                    constructors.Add(constructor);
+                }
+            }
+
+            // $constructors is reversed before iteration so that the constructor with the most common params is first instead of last
+            constructors.Reverse();
+
+            foreach (var constructor in constructors)
+            {
+                if (constructor.GetParameters().Count() <= columns.Count())
+                {
+                    return constructor;
+                }
+            }
+
+            return null;
         }
     }
 }
